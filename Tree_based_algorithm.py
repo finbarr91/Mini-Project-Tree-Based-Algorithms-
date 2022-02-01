5e86439148e994f21fc8a806ffeb19fbd2f32d79
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2022-02-01 07:51:10 -0700

    complete project

--- Tree_based_algorithm.py
+++ Tree_based_algorithm.py
@@ -1,1 +1,184 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Your code here! :)
+german_credit=pd.read_csv('GermanCredit.csv')
+print(german_credit.head()) # Dataset columns
+
+colNames=german_credit.columns
+
+#reclassify 'class' variable, 0='Bad',1='Good'
+german_credit['Class']=german_credit['Class'].replace('Good',1).replace('Bad',0)
+
+print(german_credit.head(3))
+
+y=np.array(german_credit['Class'])
+X=np.array(german_credit.drop(['Class'],axis=1))
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2, whiten=True).fit(X)
+X_pca = pca.transform(np.array(german_credit.drop(['Class'],axis=1)))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.3,random_state=3)
+
+# Visualize Boundaries
+
+
+def visualize_tree(classifier, X, y, boundaries=True, xlim=None, ylim=None):
+    '''
+    Visualizes a Decision Tree.
+    INPUTS: Classifier Model, X, y, optional x/y limits.
+    OUTPUTS: Meshgrid visualization for boundaries (boolean) of the Decision Tree
+    '''
+
+    # Fit the X and y data to the tree
+    classifier.fit(X, y)
+
+    # Automatically set the x and y limits to the data (+/- 0.1)
+    if xlim is None:
+        xlim = (X[:, 0].min() - 0.1, X[:, 0].max() + 0.1)
+    if ylim is None:
+        ylim = (X[:, 1].min() - 0.1, X[:, 1].max() + 0.1)
+
+    # Assign the variables
+    x_min, x_max = xlim
+    y_min, y_max = ylim
+
+    # Create a mesh grid
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+
+    # Define the Z by the predictions (this will color in the mesh grid)
+    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Reshape based on meshgrid
+    Z = Z.reshape(xx.shape)
+
+    # Plot the figure (use)
+    plt.figure(figsize=(10, 10))
+    plt.pcolormesh(xx, yy, Z, alpha=0.2, cmap='jet')
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='jet')
+
+    # Set Limits
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    def plot_boundaries(i, xlim, ylim):
+        '''
+        Plots the Decision Boundaries
+        '''
+        if i < 0:
+            return
+
+        # Shorter variable name
+        tree = classifier.tree_
+
+        # Recursively go through nodes of tree to plot boundaries.
+        if tree.feature[i] == 0:
+            plt.plot([tree.threshold[i], tree.threshold[i]], ylim, '-k')
+            plot_boundaries(tree.children_left[i],
+                            [xlim[0], tree.threshold[i]], ylim)
+            plot_boundaries(tree.children_right[i],
+                            [tree.threshold[i], xlim[1]], ylim)
+
+        elif tree.feature[i] == 1:
+            plt.plot(xlim, [tree.threshold[i], tree.threshold[i]], '-k')
+            plot_boundaries(tree.children_left[i], xlim,
+                            [ylim[0], tree.threshold[i]])
+            plot_boundaries(tree.children_right[i], xlim,
+                            [tree.threshold[i], ylim[1]])
+
+    # Random Forest vs Single Tree
+    if boundaries:
+        plot_boundaries(0, plt.xlim(), plt.ylim())
+
+
+clf = DecisionTreeClassifier(max_depth=7, random_state=0)
+
+# Show Boundaries
+visualize_tree(clf, X_pca, y)
+
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train,y_train)
+
+from IPython.display import Image
+from sklearn.externals.six import StringIO
+import pydotplus
+
+features = list(german_credit.columns)[:9]+list(german_credit.columns)[9+1:]
+dot_data = StringIO()
+tree.export_graphviz(clf, out_file=dot_data,  feature_names=features)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png("tree.png")
+Image(graph.create_png())
+"""
+Random Forests
+
+As discussed in the lecture videos, Decision Tree algorithms also have certain undesireable properties. Mainly the have low bias, which is good, but tend to have high variance - which is not so good (more about this problem here: https://en.wikipedia.org/wiki/Bias%E2%80%93variance_tradeoff).
+
+Noticing these problems, the late Professor Leo Breiman, in 2001, developed the Random Forests algorithm, which mitigates these problems, while at the same time providing even higher predictive accuracy than the majority of Decision Tree algorithm implementations. While the curriculum contains two excellent lectures on Random Forests, if you're interested, you can dive into the original paper here: https://link.springer.com/content/pdf/10.1023%2FA%3A1010933404324.pdf.
+
+In the next part of this assignment, your are going to use the same "German Credit" dataset to train, tune, and measure the performance of a Random Forests model. You will also see certain functionalities that this model, even though it's a bit of a "black box", provides for some degree of interpretability.
+
+First, let's build a Random Forests model, using the same best practices that you've used for your Decision Trees model. You can reuse the things you've already imported there, so no need to do any re-imports, new train/test splits, or loading up the data again.
+"""
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(n_estimators=10,random_state=0)
+
+# Get rid of boundaries to avoid error
+visualize_tree(clf,X_pca,y,boundaries=False)
+"""
+
+As mentioned, there are certain ways to "peek" into a model created by the Random Forests algorithm. The first, and most popular one, is the Feature Importance calculation functionality. This allows the ML practitioner to see an ordering of the importance of the features that have contributed the most to the predictive accuracy of the model.
+
+You can see how to use this in the scikit-learn documentation (http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier.feature_importances_). Now, if you tried this, you would just get an ordered table of not directly interpretable numeric values. Thus, it's much more useful to show the feature importance in a visual way. You can see an example of how that's done here: http://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html#sphx-glr-auto-examples-ensemble-plot-forest-importances-py
+
+Now you try! Let's visualize the importance of features from your Random Forests model!
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_classification
+from sklearn.ensemble import ExtraTreesClassifier
+
+clf.fit(X, y)
+Accuracy=clf.score(X, y)
+print('Accuracy:',Accuracy,'\n')
+
+importFeature = clf.feature_importances_
+feature_importances=pd.DataFrame([importFeature])
+
+std = np.std([tree.feature_importances_ for tree in clf.estimators_],axis=0)
+indices = np.argsort(importFeature)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(X.shape[1]), importFeature[indices],color="r", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), indices)
+plt.xlim([-1, X.shape[1]])
+plt.show()
+
+feature_importances=pd.DataFrame(pd.Series(colNames)[indices])
+feature_importances['importance']=np.sort(importFeature)[::-1]
+feature_importances.columns=['features','importance']
+feature_importances
+
+(feature_importances[:5]).plot(kind='barh')
+plt.title('Feature Importances')
+
+
 
